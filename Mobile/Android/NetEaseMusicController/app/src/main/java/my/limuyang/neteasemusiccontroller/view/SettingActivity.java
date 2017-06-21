6461b19fc35cb4c1e81db472d9c3a867b61dea53
limuyang <limuyang2@live.com>
2017-06-22 00:44:12 +0800

    删除不用的开源库

--- Mobile/Android/NetEaseMusicController/app/src/main/java/my/limuyang/neteasemusiccontroller/view/SettingActivity.java
+++ Mobile/Android/NetEaseMusicController/app/src/main/java/my/limuyang/neteasemusiccontroller/view/SettingActivity.java
@@ -1,8 +1,11 @@
 package my.limuyang.neteasemusiccontroller.view;
 
+
+import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import my.limuyang.neteasemusiccontroller.R;
 
@@ -18,9 +21,24 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(getString(R.string.setting));
+
+
+
         getFragmentManager().beginTransaction()
                 .replace(R.id.setting_frameLayout, new SettingPreferenceFragment())
                 .commit();
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
